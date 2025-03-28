b02c6899434993970f5e05d946d93099b0c98246
ArchLinuxChad <thedillonking@proton.me>
2025-03-28 21:29:37 +0000

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,11 @@ alias cls="clear"
 alias ls="eza -aal"
 
 alias rm="trash -v"
+
+alias cat="bat"
+
+# aliases for git
+
+alias addup="git add ."
+alias commit="git commit -m"
+alias push="git push -u origin main"
