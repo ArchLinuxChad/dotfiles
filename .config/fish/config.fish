8d83571162dc2f1c148ef72a923ee8884b9c43c5
ArchLinuxChad <thedillonking@proton.me>
2025-03-28 21:19:59 +0000

    added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,11 @@ alias cls="clear"
 alias ls="eza -aal"
 
 alias rm="trash -v"
+
+alias bat="batcat"
+
+alias cat="bat"
+
+alias gtd="cd ~/github-repos/dotfiles"
+alias gtc="cd ~/github-repos/dotfiles/.config"
+alias gts="cd ~/github-repos/scripts"
